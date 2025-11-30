80ea80566e9d913ce1ac60d935607458c480a26d
Mosiah87 <mosiah87@gmail.com>
2025-11-29 20:23:40 -0700

    Update name.js

--- number-guessing-app/frontend/src/components/name.js
+++ number-guessing-app/frontend/src/components/name.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import "bootstrap/dist/css/bootstrap.css";
-
+ 
 let insertedId;
 
 export default function Name() {
